de7c2818f179ec0e6aa3981e8ddaf4038e68abb5
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-04-02 22:31:04 -0300

    update carro.java

--- src/main/java/br/com/estacione/fiap/entity/Carro.java
+++ src/main/java/br/com/estacione/fiap/entity/Carro.java
@@ -1,5 +1,31 @@
 package br.com.estacione.fiap.entity;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@ToString
+@Table(name="tb_carro")
+
 public class Carro {
+
+    @Id
+    @Column(columnDefinition = "UUID")
+    private UUID uuid;
+
+    @Column(nullable = false, unique = true, length = 8)
+    private String modelo;
+
+    @Column(nullable = false, unique = true, length = 8)
+    private String placa;
+
 
 }
