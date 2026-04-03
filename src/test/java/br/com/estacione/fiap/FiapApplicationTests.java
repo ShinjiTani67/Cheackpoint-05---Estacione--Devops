4cddc99dc7558545ee7280d2efee360d689d8bc7
Fernando Tanigushi <144123611+ShinjiTani67@users.noreply.github.com>
2026-04-03 17:42:31 -0300

    Disable SpringBootTest and contextLoads test
    Comment out SpringBootTest and Test annotations.

--- src/test/java/br/com/estacione/fiap/FiapApplicationTests.java
+++ src/test/java/br/com/estacione/fiap/FiapApplicationTests.java
@@ -3,10 +3,10 @@ package br.com.estacione.fiap;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class FiapApplicationTests {
 
-	@Test
+	//@Test
 	void contextLoads() {
 	}
 
